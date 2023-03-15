83b229ee36f14992272ca998f563a25d5ca099b9
Giura Emanuel <emanuel1996a@gmail.com>
2023-03-15 16:36:39 +0200

    small changes

--- Source/Customer-RegistrationWCF.BL/Data/Address.cs
+++ Source/Customer-RegistrationWCF.BL/Data/Address.cs
@@ -9,9 +9,8 @@
 
 namespace Customer_RegistrationWCF.BL.Data
 {
-    using System;
     using System.Collections.Generic;
-    
+
     public partial class Address
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
